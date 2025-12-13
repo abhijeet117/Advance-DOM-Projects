21df31f4b2eacf94aaf27550ad650de123799b7b
abhijeet117 <abhijeetkumarsah92@gmail.com>
2025-12-13 08:09:53 +0530

    created the logic for input field

--- weather-checker/script.js
+++ weather-checker/script.js
@@ -1,3 +1,14 @@
+let input = document.querySelector('.weather-input');
+let btn = document.querySelector('.search-btn');
+
+
+let temp = document.querySelector('.temperature');
+let cityname = document.querySelector('.city-name');
+let status = document.querySelector('.weather-status');
+let humidity = document.querySelector('.humidity');
+let windspeed = document.querySelector('.wind-speed');
+
+
 async function fetchWeather(city) {
   let apikey = `49e69facae4d7ff8cab736c12b2bd0d6`;
   let raw = await fetch(
@@ -5,8 +16,19 @@ async function fetchWeather(city) {
   );
   let original = await raw.json();
   console.log(original);
+
+  console.log(original.main.temp)
+
+  btn.addEventListener('click', function() {
+    console.log("hii")
+  });
+
 }
 
-fetchWeather("Teghra");
+
+btn.addEventListener('click', function() {
+  fetchWeather(input.value);
+})
+
 
 
